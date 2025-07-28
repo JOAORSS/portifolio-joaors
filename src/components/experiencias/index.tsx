fdd15d813517120807e13f8aa136ba108e221fa2
JoaoRS <joaors987654321@gmail.com>
2025-07-28 14:15:12 -0300

    scale down da imagem experiencia

--- src/components/experiencias/index.tsx
+++ src/components/experiencias/index.tsx
@@ -11,32 +11,32 @@ export default function Experiencias() {
                 <h2 className={styles.title}>Experiência</h2>
                 <div className={styles.experiencesGrid}>
                     <ExperienciasCard
-                    title="Desenvolvedor Frontend"
-                    company="Domínio Fatal - Freelancer"
-                    time="Out 2024 - Mar 2025"
-                    description="Desenvolvi as principais telas e funcionalidades de um site e-commerce para a Domínio Fatal, atuando desde o design até a implementação. O projeto foi interrompido por motivos alheios ao desenvolvimento."
+                        title="Desenvolvedor Frontend"
+                        company="Domínio Fatal - Freelancer"
+                        time="Out 2024 - Mar 2025"
+                        description="Desenvolvi as principais telas e funcionalidades de um site e-commerce para a Domínio Fatal, atuando desde o design até a implementação. O projeto foi interrompido por motivos alheios ao desenvolvimento."
                     />
                     <ExperienciasCard
-                    title="TI - Tribunal de Justiça"
-                    company="Terceirizado - Microempreendedor Individual"
-                    time="Mai 2025 - Presente"
-                    description="Responsável pela instalação e configuração de novos computadores, suporte aos servidores em rotinas de backup, resolução de problemas de rede e impressoras, além de ajustes pontuais em servidores, como configuração de DHCP e gerenciamento de endereços IP."
+                        title="TI - Tribunal de Justiça"
+                        company="Terceirizado - Microempreendedor Individual"
+                        time="Mai 2025 - Presente"
+                        description="Responsável pela instalação e configuração de novos computadores, suporte aos servidores em rotinas de backup, resolução de problemas de rede e impressoras, além de ajustes pontuais em servidores, como configuração de DHCP e gerenciamento de endereços IP."
                     />
                     <Image
-                    src="/dominio_fatal.jpg"
-                    alt="Experiências"
-                    width={600}
-                    height={500}
-                    style={{
-                        width: "100%",
-                        maxWidth: "600px",
-                        height: "340px",
-                        borderRadius: "10px",
-                        aspectRatio: "19/9",
-                        objectFit: "cover",
-                        objectPosition: "top",
-                        display: "block"
-                    }}
+                        src="/dominio_fatal.jpg"
+                        alt="Experiências"
+                        width={500}
+                        height={400}
+                        style={{
+                            width: "100%",
+                            maxWidth: "600px",
+                            height: "340px",
+                            borderRadius: "10px",
+                            aspectRatio: "19/9",
+                            objectFit: "cover",
+                            objectPosition: "top",
+                            display: "block"
+                        }}
                     />
                     <Image
                     src="https://media.independente.com.br/images/2024/06/forum-de-justica-de-lajeadojpeg-1718602929.jpeg"
